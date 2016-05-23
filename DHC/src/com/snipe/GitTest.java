a2a0ea598a878511dc0ebf013f0762850a7ac368
Raj <Raj@BasaveshwarMahaRAJ--ki-Jai>
2016-05-23 15:41:51 +0530

    monday commit

---
+++ DHC/src/com/snipe/GitTest.java
@@ -0,0 +1,17 @@
+package com.snipe;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		System.out.println("monday operation");
+		
+		
+		
+		
+
+		
+
+	}
+
+}
